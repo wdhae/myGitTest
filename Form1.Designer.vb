54ecc78444177479f7e48a434d0303ac8b795141
wdhae <willem@tcmb.eu>
2012-01-04 13:47:54 +0100

    added calendar

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -28,6 +28,9 @@ Partial Class Form1
         Me.RadioButton1 = New System.Windows.Forms.RadioButton
         Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox
         Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker
+        Me.MonthCalendar1 = New System.Windows.Forms.MonthCalendar
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
         Me.SuspendLayout()
         '
         'Label1
@@ -55,6 +58,7 @@ Partial Class Form1
         Me.ListBox1.TabIndex = 2
         '
         'RadioButton1
+        'MonthCalendar1
         '
         Me.RadioButton1.AutoSize = True
         Me.RadioButton1.Location = New System.Drawing.Point(15, 177)
@@ -78,6 +82,28 @@ Partial Class Form1
         Me.DateTimePicker1.Location = New System.Drawing.Point(288, 102)
         Me.DateTimePicker1.Name = "DateTimePicker1"
         Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.MonthCalendar1.Location = New System.Drawing.Point(18, 166)
+        Me.MonthCalendar1.Name = "MonthCalendar1"
+        Me.MonthCalendar1.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(201, 305)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(202, 273)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         Me.DateTimePicker1.TabIndex = 5
         '
         'Form1
@@ -88,6 +114,10 @@ Partial Class Form1
         Me.Controls.Add(Me.DateTimePicker1)
         Me.Controls.Add(Me.CheckedListBox1)
         Me.Controls.Add(Me.RadioButton1)
+        Me.ClientSize = New System.Drawing.Size(343, 346)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MonthCalendar1)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
@@ -103,5 +133,8 @@ Partial Class Form1
     Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
     Friend WithEvents CheckedListBox1 As System.Windows.Forms.CheckedListBox
     Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents MonthCalendar1 As System.Windows.Forms.MonthCalendar
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
 
 End Class
